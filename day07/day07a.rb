db631404f3a54447e90313a1d6c0d89e57fd5a03
Johannes Opper <xijo@pm.me>
2020-12-07 11:03:45 +0100

    day 07

---
+++ day07/day07a.rb
@@ -0,0 +1,24 @@
+require 'fileutils'
+
+tree = {}
+
+File.read('input').each_line do |line|
+  bag = line[/\A\w+\s\w+/]
+  tree[bag] = {}
+
+  if line.end_with?('contain no other bags')
+  else
+    line.scan(/(\d+\s\w+\s\w+)/).each do |match|
+      tree[bag][match.first[/\w+\s\w+\z/]] = match.first[/\d+/].to_i
+    end
+  end
+end
+
+def contains_shiny_gold?(bag, tree)
+  return true if tree[bag].keys.include?('shiny gold')
+  return false if tree[bag].keys.empty?
+
+  tree[bag].keys.any? { |i| contains_shiny_gold?(i, tree) }
+end
+
+puts tree.keys.select { |i| contains_shiny_gold?(i, tree) }.size
---
+++ day07/day07b.rb
@@ -0,0 +1,25 @@
+require 'fileutils'
+
+tree = {}
+
+File.read('input').each_line do |line|
+  bag = line[/\A\w+\s\w+/]
+  tree[bag] = {}
+
+  if line.end_with?('contain no other bags')
+  else
+    line.scan(/(\d+\s\w+\s\w+)/).each do |match|
+      tree[bag][match.first[/\w+\s\w+\z/]] = match.first[/\d+/].to_i
+    end
+  end
+end
+
+def bag_count(bag, tree)
+  return 1 if tree[bag].keys.empty?
+
+  tree[bag].inject(1) do |i, (k, v)|
+    i + (bag_count(k, tree) * v)
+  end
+end
+
+puts bag_count('shiny gold', tree) - 1
